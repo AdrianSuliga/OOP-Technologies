00fcec87f7e61ddb2edb45fd35fd2f38712c05c9
Adrian Suliga <adriansuliga81@gmail.com>
2025-10-25 21:48:57 +0200

    Add Active Record lab

---
+++ Lab1/active-record/src/main/java/pl/edu/agh/iisg/to/model/Course.java
@@ -0,0 +1,140 @@
+package pl.edu.agh.iisg.to.model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.logging.Logger;
+
+import pl.edu.agh.iisg.to.executor.QueryExecutor;
+
+public class Course {
+
+    public static final String TABLE_NAME = "course";
+
+    private static final Logger logger = Logger.getGlobal();
+
+    private final int id;
+
+    private final String name;
+
+    private List<Student> enrolledStudents;
+
+    private boolean isStudentsListDownloaded = false;
+
+    Course(final int id, final String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public static Optional<Course> create(final String name) {
+        String insertSql = "INSERT INTO course (name) VALUES (?);";
+        Object[] args = {
+                name
+        };
+
+        try {
+            int id = QueryExecutor.createAndObtainId(insertSql, args);
+            return Course.findById(id);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return Optional.empty();
+    }
+
+    public static Optional<Course> findById(final int id) {
+        String findByIdSql = "SELECT * FROM course WHERE id = ?";
+        Object[] args = {
+                id
+        };
+
+        try (ResultSet rs = QueryExecutor.read(findByIdSql, args)) {
+            if (rs.next()) {
+                return Optional.of(new Course(
+                        rs.getInt("id"),
+                        rs.getString("name")
+                ));
+            } else {
+                return Optional.empty();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return Optional.empty();
+    }
+
+    public boolean enrollStudent(final Student student) {
+        String enrollStudentSql = "INSERT INTO student_course (student_id, course_id) VALUES (?, ?);";
+
+        try {
+            QueryExecutor.createAndObtainId(enrollStudentSql, student.id(), id());
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public List<Student> studentList() {
+        String findStudentListSql = "SELECT * FROM student AS s INNER JOIN student_course AS sc ON s.id = sc.student_id WHERE sc.course_id = ?;";
+
+        try {
+            ResultSet resultSet = QueryExecutor.read(findStudentListSql, id());
+
+            List<Student> resultList = new LinkedList<>();
+            while (resultSet.next()) {
+                resultList.add(new Student(resultSet.getInt("id"), resultSet.getString("first_name"),
+                        resultSet.getString("last_name"), resultSet.getInt("index_number")));
+            }
+
+            return resultList;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public List<Student> cachedStudentsList() {
+        if (!this.isStudentsListDownloaded) {
+            this.enrolledStudents = this.studentList();
+            this.isStudentsListDownloaded = true;
+        }
+
+        return this.enrolledStudents;
+    }
+
+    public int id() {
+        return id;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public static class Columns {
+
+        public static final String ID = "id";
+
+        public static final String NAME = "name";
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Course course = (Course) o;
+
+        if (id != course.id) return false;
+        return name.equals(course.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + name.hashCode();
+        return result;
+    }
+}
